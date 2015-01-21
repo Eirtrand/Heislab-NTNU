0184353bd026c0d3074d9e395a679ac02e0037c9
neweirik <neweirik@gmail.com>
2015-01-21 08:54:40 +0100

    oving 2 mutex fngerer

--- Oving2/helloworld_c.c
+++ Oving2/helloworld_c.c
@@ -1,62 +1,53 @@
-
 // gcc 4.7.2 +
 // gcc -std=gnu99 -Wall -g -o helloworld_c helloworld_c.c -lpthread
 #include <pthread.h>
 #include <stdio.h>
-#include <stdint.h>
-
-	pthread_mutex_t lock;
-	pthread_mutex_t lock2;
-
-void adder();
-void adder2();
+// Note the return type: void*
+int i= 0;
+pthread_mutex_t lock;
 
 
-int i = 0;
+void* someThreadFunction1(){
+for(int k = 0;k<1000000;k++){
+	pthread_mutex_lock(&lock);
+	i++;
+	pthread_mutex_unlock(&lock);
+}
+return NULL;
+}
+
+void* someThreadFunction2(){
+for(int k = 0;k<1000000;k++){
+	pthread_mutex_lock(&lock);
+	i--;
+	pthread_mutex_unlock(&lock);
+}
+return NULL;
+}
 
 
+int main(){
+	if (pthread_mutex_init(&lock, NULL) != 0)
+    {
+        printf("\n mutex init failed\n");
+        return 1;
+    }
 
+	pthread_t Thread1;
+	pthread_t Thread2;
 
-int main() {
+	pthread_create(&Thread1, NULL, someThreadFunction1, &lock);
+	pthread_create(&Thread2, NULL, someThreadFunction2, &lock);
+	// Arguments to a thread would be passed here ---------^
+	pthread_join(Thread1, NULL);
+	pthread_join(Thread2, NULL);
 
-
-	pthread_mutex_init(&lock, NULL);
-	pthread_mutex_init(&lock2, NULL);
-
-	pthread_t adder_thr;
-	pthread_t subst_thr;
-
-	pthread_create(&adder_thr, NULL, adder2, NULL );
-	pthread_create(&subst_thr, NULL, adder, NULL );
-
-	pthread_join(adder_thr, NULL);
-	pthread_join(subst_thr, NULL);
-	pthread_mutex_destroy(&lock2);
 	pthread_mutex_destroy(&lock);
-	printf("%i\n", i);
 
-	return 0;
+	printf("%d\n",i);
+return 0;
 }
 
 
-void adder() {
-	for(int x = 0; x < 1000000; x++){
 
-		pthread_mutex_lock(&lock);
-		i--;
-		pthread_mutex_unlock(&lock);
 
-	}
-	return NULL;
-}
-
-void adder2() {
-	for(int x = 0; x < 1000000; x++){
-
-		pthread_mutex_lock(&lock2);
-		i++;
-		pthread_mutex_unlock(&lock2);
-		
-	}
-	return NULL;
-}
