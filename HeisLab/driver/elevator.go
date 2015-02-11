d256a1645a3ddcd9043920c8981dee0aadffcecc
alexagv <alexagv@stud.ntnu.no>
2015-02-11 13:30:57 +0100

    yolo

--- HeisLab/driver/elevator.go
+++ HeisLab/driver/elevator.go
@@ -2,7 +2,7 @@ package driver
 
 import "fmt"
 
-func initialize_elevator() {
+func initialize_elevator(chan floor_chan int, chan button_chan int) {
 	if !Init() int {
 		return 0
 	}
@@ -12,4 +12,37 @@ func initialize_elevator() {
 		}
 	}
 
+}
+
+func poll_all_signals() {
+	for {
+		get_floor_sensor()
+		get_button_signals()
+
+		
+	}
+	
+}
+
+func get_floor_sensor_signal() {
+	if Read_bit(SENSOR_FLOOR1) {
+		return 1
+	}
+	else if Read_bit(SENSOR_FLOOR2) {
+		return 2
+	}
+	else if Read_bit(SENSOR_FLOOR3) {
+		return 3
+	}
+	else if Read_bit(SENSOR_FLOOR4) {
+		return 4
+	}
+	else {
+		return 0
+	}
+	
+}
+
+func get_button_signals() {
+	
 }
